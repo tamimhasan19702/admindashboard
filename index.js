c440325957932eb35c12f0e51596eeb4e176f66f
tamimhasan19702 <tamimhasancu@gmail.com>
2022-04-03 00:13:16 +0600

    c

--- index.js
+++ index.js
@@ -13,4 +13,8 @@ closeBtn.addEventListener('click',() => {
 
 themeToggler.addEventListener('click',() =>{
     document.body.classList.toggle('dark-theme-variables');
+   
+    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
+    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
+  
 })
